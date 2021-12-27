9aa25427b94ff9c333cb99d33e4ad1fb2363fc1a
김시우 <tldn12gh@gmail.com>
2021-12-28 00:58:55 +0900

    보초를 사용한 배열의 선형탐색

---
+++ src/Chapter03/SeqSearchSentinel.java
@@ -0,0 +1,38 @@
+package Chapter03;
+
+import java.util.Scanner;
+
+public class SeqSearchSentinel {
+
+    static int seqSearchSentinel(int[] a, int key, int num) {
+        a[num] = key;
+        int i = 0;
+        while (true) {
+            if (a[i] == key) {
+                break;
+            }
+            i++;
+        }
+        return i == num ? -1 : i;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수를 입력하세요");
+        int num = stdIn.nextInt();
+        int[] a = new int[num + 1];
+
+        for (int i = 0; i < num; i++) {
+            System.out.print("a[" + i + "] : ");
+            a[i] = stdIn.nextInt();
+        }
+
+        System.out.print("찾고자 하는 KEY :");
+        int key = stdIn.nextInt();
+
+        int idx = seqSearchSentinel(a, key, num);
+        if (idx == -1) System.out.println("찾고자 하는 값이 없습니다.");
+        else System.out.println("찾고자 하는 값이 a[" + idx + "]에 있습니다.");
+    }
+}
