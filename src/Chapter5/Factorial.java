14c1945d11e6b82810ac6172335923df5cc65f26
김시우 <tldn12gh@gmail.com>
2022-01-06 00:49:14 +0900

    재귀를 하지 않은 팩토리얼

---
+++ src/Chapter5/Factorial.java
@@ -0,0 +1,22 @@
+package Chapter5;
+
+import java.util.Scanner;
+
+public class Factorial {
+    static int factorial(int x) {
+        int fac = 1;
+        for(int i = 1; i <= x; i++) {
+            fac *= i;
+        }
+        return fac;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("x ? :");
+        int x = stdIn.nextInt();
+
+        System.out.println("팩토리얼의 결과값은 " + factorial(x) + "입니다.");
+    }
+}
