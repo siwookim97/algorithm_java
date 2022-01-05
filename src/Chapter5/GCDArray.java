7fe9042a10f490ed13b447dfac54074f9d189f5a
김시우 <tldn12gh@gmail.com>
2022-01-06 01:27:57 +0900

    (수정중) 배열 요소의 최대 공약수 구하는 알고리즘

---
+++ src/Chapter5/GCDArray.java
@@ -0,0 +1,35 @@
+package Chapter5;
+
+import java.util.Scanner;
+
+public class GCDArray {
+    static int gcd(int x, int y) {
+        int temp;
+        while (y != 0) {
+            temp = y;
+            y = x % y;
+            x = temp;
+        }
+        return x;
+    }
+
+    static int gcdArray(int[] a) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = stdIn.nextInt();
+        int[] a = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.print((i + 1) + "번째 : ");
+            a[i] = stdIn.nextInt();
+        }
+
+        System.out.println("최대 공약수의 결과는 " + gcdArray(a) + "입니다.");
+    }
+}
