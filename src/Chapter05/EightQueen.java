080819e03eb3634c77cbf0465484b9d50e08ebf4
김시우 <tldn12gh@gmail.com>
2022-01-06 23:55:09 +0900

    8개의 퀸을 서로 잡히지 않게 배치하는 알고리즘

---
+++ src/Chapter05/EightQueen.java
@@ -0,0 +1,42 @@
+package Chapter05;
+
+public class EightQueen {
+    static int[] pos = new int[8];
+    static boolean[] flag_a = new boolean[8]; // 행 뱡향 대각선 중복확인
+    static boolean[] flag_b = new boolean[15]; // / 방향 대각선 중복확인
+    static boolean[] flag_c = new boolean[15]; // \ 방향 대각선 중복확인
+
+    static void print() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (j == pos[i])
+                    System.out.print("7 ");
+                else
+                    System.out.print("0 ");
+            }
+            System.out.println();
+
+        }
+        System.out.println();
+    }
+
+    static void set(int i) {
+        for (int j = 0; j < 8; j++) {
+            if (flag_a[j] == false && flag_b[i + j] == false && flag_c[i - j + 7] == false) {
+                pos[i] = j;
+
+                if (i == 7)
+                    print();
+                else {
+                    flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = true;
+                    set(i + 1);
+                    flag_a[j] = flag_b[i + j] = flag_c[i - j + 7] = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        set(0);
+    }
+}
