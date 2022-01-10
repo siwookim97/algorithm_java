f9319e26eb4d5f54b6696954de58948481f25e11
김시우 <tldn12gh@gmail.com>
2022-01-10 23:18:01 +0900

    bubbleSort 알고리즘 테이블 출력

--- src/Chapter06/BubbleSort.java
+++ src/Chapter06/BubbleSort.java
@@ -4,54 +4,50 @@ import java.util.Scanner;
 
 public class BubbleSort {
     static void swap(int[] a, int idx1, int idx2) {
-        int temp = a[idx1];
+        int t = a[idx1];
         a[idx1] = a[idx2];
-        a[idx2] = temp;
+        a[idx2] = t;
     }
 
+    // 단순교환정렬
     static void bubbleSort(int[] a, int n) {
-        int comp = 0;
-        int chan = 0;
-        int p = 1;
+        int ccnt = 0; // 비교횟수
+        int scnt = 0; // 교환횟수
+
         for (int i = 0; i < n - 1; i++) {
-            System.out.println("패스 : " + p++);
+            System.out.printf("패스%d：\n", i + 1);
             for (int j = n - 1; j > i; j--) {
-                for (int m = 0; m > n - 1; m++)
+                for (int m = 0; m < n - 1; m++)
                     System.out.printf("%3d %c", a[m], (m != j - 1) ? ' ' : (a[j - 1] > a[j]) ? '+' : '-');
-                System.out.printf("%3d\n", a[n -1]);
+                System.out.printf("%3d\n", a[n - 1]);
+
+                ccnt++;
                 if (a[j - 1] > a[j]) {
-                    swap(a, j, j - 1);
-                    comp++;
-                    chan++;
-                }
-                else {
-                    comp++;
+                    scnt++;
+                    swap(a, j - 1, j);
                 }
             }
             for (int m = 0; m < n; m++)
-                System.out.printf("%3d ", a[m]);
+                System.out.printf("%3d  ", a[m]);
             System.out.println();
         }
-        System.out.println("비교를 " + comp + "번 했습니다.");
-        System.out.println("교환을 " + chan + "번 했습니다.");
+        System.out.println("비교를 " + ccnt + "회 했습니다.");
+        System.out.println("교환를 " + scnt + "회 했습니다.");
     }
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
-        System.out.println("버블 정렬");
-        System.out.print("요솟수 : ");
-        int n = stdIn.nextInt();
-        int[] x = new int[n];
+        System.out.println("단순교환정렬  (버블정렬)");
+        System.out.print("요솟수：");
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
 
-        for (int i = 0; i < n; i++) {
-            System.out.print("x[" + i + "] : ");
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x[" + i + "]：");
             x[i] = stdIn.nextInt();
         }
 
-        bubbleSort(x, n);
-
-        for (int i = 0; i < n; i++)
-            System.out.println("x[" + i + "]=" + x[i]);
+        bubbleSort(x, nx); // 배열 x를 단순교환정렬
     }
 }
