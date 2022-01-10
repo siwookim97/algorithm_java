10acbf6cba36b98cd61d971c33e72896a66e810a
김시우 <tldn12gh@gmail.com>
2022-01-10 23:17:26 +0900

    bubbleSort 알고리즘 테이블 출력

--- src/Chapter06/BubbleSort.java
+++ src/Chapter06/BubbleSort.java
@@ -10,13 +10,33 @@ public class BubbleSort {
     }
 
     static void bubbleSort(int[] a, int n) {
-        for (int i = 0; i < n - 1; i++)
-            for (int j = 0; j < n - 1; j++)
-                if (a[j + 1] < a[j])
-                    swap(a, j, j + 1);
+        int comp = 0;
+        int chan = 0;
+        int p = 1;
+        for (int i = 0; i < n - 1; i++) {
+            System.out.println("패스 : " + p++);
+            for (int j = n - 1; j > i; j--) {
+                for (int m = 0; m > n - 1; m++)
+                    System.out.printf("%3d %c", a[m], (m != j - 1) ? ' ' : (a[j - 1] > a[j]) ? '+' : '-');
+                System.out.printf("%3d\n", a[n -1]);
+                if (a[j - 1] > a[j]) {
+                    swap(a, j, j - 1);
+                    comp++;
+                    chan++;
+                }
+                else {
+                    comp++;
+                }
+            }
+            for (int m = 0; m < n; m++)
+                System.out.printf("%3d ", a[m]);
+            System.out.println();
+        }
+        System.out.println("비교를 " + comp + "번 했습니다.");
+        System.out.println("교환을 " + chan + "번 했습니다.");
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
         System.out.println("버블 정렬");
@@ -31,7 +51,6 @@ public class BubbleSort {
 
         bubbleSort(x, n);
 
-        System.out.println("오름차순으로 정렬 완료");
         for (int i = 0; i < n; i++)
             System.out.println("x[" + i + "]=" + x[i]);
     }
