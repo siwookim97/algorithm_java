4a2c5d298d5a1d61f93b2ebf895a1de18fdaa349
김시우 <tldn12gh@gmail.com>
2022-01-11 22:42:38 +0900

    단순 삽입 정렬 알고리즘

--- src/Chapter06/InsertionSort.java
+++ src/Chapter06/InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InsertionSort {
     static void insertionSort(int[] a, int n) {
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) { // 0이 아닌 1번째 index부터 시작한다
             int j;
             int tmp = a[i];
             for (j = i; j > 0 && a[j - 1] > tmp; j--)
