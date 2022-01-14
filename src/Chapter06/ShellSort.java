724c7b294ff985eca372fc7f4738ddad130f27cc
김시우 <tldn12gh@gmail.com>
2022-01-14 23:19:17 +0900

    셸 정렬 알고리즘 (ver.2인 shellSort2는 더 나은 요소의 이동 횟수를 보여준다)

--- src/Chapter06/ShellSort.java
+++ src/Chapter06/ShellSort.java
@@ -4,20 +4,44 @@ import java.util.Scanner;
 
 public class ShellSort {
     static void shellSort(int[] a, int n) {
-        for (int h = n / 2; h > 0; h /= 2)
+        int count = 0;
+        for (int h = n / 2; h > 0; h /= 2) // 4-정렬 ~ 2-정렬 ~ 1-정렬 순서처럼 정렬
             for (int i = h; i < n; i++) {
                 int j;
                 int tmp = a[i];
-                for (j = i - h; j >= 0 && a[j] > tmp; j -= h)
+                for (j = i - h; j >= 0 && a[j] > tmp; j -= h) {
                     a[j + h] = a[j];
+                    count++;
+                }
                 a[j + h] = tmp;
+                count++;
             }
+        System.out.println("요소의 이동 횟수 : " + count);
+    }
+
+    static void shellSort2(int[] a, int n) {
+        int count = 0;
+        int h;
+        for (h = 1; h < n / 9; h = h * 3 + 1) ;
+
+        for (; h > 0; h /= 3)
+            for (int i = h; i < n; i++) {
+                int j;
+                int tmp = a[i];
+                for (j = i - h; j >= 0 && a[j] > tmp; j -= h) {
+                    a[j + h] = a[j];
+                    count++;
+                }
+                a[j + h] = tmp;
+                count++;
+            }
+        System.out.println("요소의 이동 횟수 : " + count);
     }
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
-        System.out.println("셸 정렬(버전 1)");
+        System.out.println("셸 정렬(버전 2)");
         System.out.print("요솟수 : ");
         int nx = stdIn.nextInt();
         int[] x = new int[nx];
@@ -27,7 +51,7 @@ public class ShellSort {
             x[i] = stdIn.nextInt();
         }
 
-        shellSort(x, nx);
+        shellSort2(x, nx);
 
         System.out.println("오름차순으로 정렬.");
         for (int i = 0; i < nx; i++)
