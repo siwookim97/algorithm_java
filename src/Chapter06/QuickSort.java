f56058124ee60da6f7bf1a074b0fed728ec66ff0
김시우 <tldn12gh@gmail.com>
2022-01-18 23:07:16 +0900

    퀵 정렬 알고리즘 / quickSort3 메서드는 요소의 개수가 적은 배열은 빠른 처리를 위해서 단순 삽입 정렬로 동작하는 메서드로 수정

--- src/Chapter06/QuickSort.java
+++ src/Chapter06/QuickSort.java
@@ -60,8 +60,7 @@ public class QuickSort {
                     lstack.push(pl);
                     rstack.push(right);
                 }
-            }
-            else {
+            } else {
                 if (pl < right) {
                     lstack.push(pl);
                     rstack.push(right);
@@ -74,6 +73,57 @@ public class QuickSort {
         }
     }
 
+    static void quickSort3(int[] a, int left, int right) {
+
+        IntStack lstack = new IntStack(right - left + 1);
+        IntStack rstack = new IntStack(right - left + 1);
+
+        lstack.push(left);
+        rstack.push(right);
+
+        while (lstack.isEmpty() != true) {
+            if (right - left < 9) {
+                for (int i = left + 1; i <= right; i++) {
+                    int tmp = a[i];
+                    int j;
+                    for (j = i; j > left && a[j - 1] > tmp; j--)
+                        a[j] = a[j - 1];
+                    a[j] = tmp;
+                }
+            } else {
+                int pl = left = lstack.pop();
+                int pr = right = rstack.pop();
+                int x = a[(right + left) / 2];
+
+                do {
+                    while (a[pl] < x) pl++;
+                    while (a[pr] > x) pr--;
+                    if (pl <= pr)
+                        swap(a, pl++, pr--);
+                } while (pl <= pr);
+
+                if (pr - left <= right - pl) { // 스택에 쌓이는 데이터의 수를 줄이 기위한 조건문
+                    if (left < pr) {
+                        lstack.push(left);
+                        rstack.push(pr);
+                    }
+                    if (pl < right) {
+                        lstack.push(pl);
+                        rstack.push(right);
+                    }
+                } else {
+                    if (pl < right) {
+                        lstack.push(pl);
+                        rstack.push(right);
+                    }
+                    if (pr > left) {
+                        lstack.push(left);
+                        rstack.push(pr);
+                    }
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
@@ -83,12 +133,12 @@ public class QuickSort {
         int nx = stdIn.nextInt();
         int[] a = new int[nx];
 
-        for (int i = 0; i < nx; i++){
+        for (int i = 0; i < nx; i++) {
             System.out.print("a[" + i + "] : ");
             a[i] = stdIn.nextInt();
         }
 
-        quickSort2(a, 0, nx - 1);
+        quickSort3(a, 0, nx - 1);
 
         System.out.println("오름차순으로 정렬 완료");
         for (int i = 0; i < nx; i++)
