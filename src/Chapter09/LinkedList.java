3a0a9c0cdaa6d3bb74e813ac1fcbf4bca50e407c
김시우 <tldn12gh@gmail.com>
2022-01-30 23:51:11 +0900

    포인터로 연결 리스트

--- src/Chapter09/LinkedList.java
+++ src/Chapter09/LinkedList.java
@@ -1,5 +1,7 @@
 package Chapter09;
 
+import java.util.Comparator;
+
 public class LinkedList<E> {
     class Node<E> {
         private E data; // 데이터
@@ -13,4 +15,60 @@ public class LinkedList<E> {
 
     private Node<E> head; // 머리 노드
     private Node<E> crnt; // 선택 노드
+
+    public LinkedList() {
+        head = crnt = null;
+    }
+
+    public E search(E obj, Comparator<? super E> c) {
+        Node<E> ptr = head;
+
+        while (ptr != null) {
+            if (c.compare(obj, ptr.data) == 0) {
+                crnt = ptr;
+                return ptr.data;
+            }
+            ptr = ptr.next;
+        }
+        return null;
+    }
+
+    public void addFirst(E obj) {
+        Node<E> ptr = head;
+        head = crnt = new Node<E>(obj, ptr);
+    }
+
+    public void addLast(E obj) {
+        if (head == null)
+            addFirst(obj);
+        else {
+            Node<E> ptr = head;
+            while (ptr.next != null)
+                ptr = ptr.next;
+            ptr.next = crnt = new Node<E>(obj, null);
+        }
+    }
+
+    public void removeFirst() {
+        if (head != null)
+            head = crnt = head.next;
+    }
+
+    public void removeLast() {
+        if (head != null) {
+            if (head.next == null)
+                removeFirst();
+            else {
+                Node<E> ptr = head;
+                Node<E> pre = head;
+
+                while (ptr.next != null) {
+                    pre = ptr;
+                    ptr = ptr.next;
+                }
+                pre.next = null;
+                crnt = pre;
+            }
+        }
+    }
 }
