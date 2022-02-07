72bca8d9df54844b7fffeda5052d59df969b3b25
김시우 <tldn12gh@gmail.com>
2022-02-07 23:08:06 +0900

    배열을 활용한 연결 리스트 구현

--- src/Chapter09/AryLinkedList.java
+++ src/Chapter09/AryLinkedList.java
@@ -7,7 +7,7 @@ public class AryLinkedList<E> {
     class Node<E> {
         private E data;
         private int next;
-        private int dnext;
+        private int dnext; // 프리 리스트 다음 커서 처음은 null으로 할당
 
         void set(E data, int next) {
             this.data = data;
@@ -15,13 +15,13 @@ public class AryLinkedList<E> {
         }
     }
 
-    private Node<E>[] n;
-    private int size;
-    private int max;
-    private int head;
-    private int crnt;
-    private int deleted;
-    private static final int NULL = -1;
+    private Node<E>[] n;  // 리스트 본체
+    private int size;  // 리스트의 용량
+    private int max;  // 사용 중인 꼬리 record
+    private int head;  // 머리 노드
+    private int crnt;  // 선택 노드
+    private int deleted;  // free 리스트의 머리 노드
+    private static final int NULL = -1;  // 다음 노드 없음 / 리스트가 가득 참
 
 
     public AryLinkedList(int capacity) {
@@ -51,7 +51,7 @@ public class AryLinkedList<E> {
         }
     }
 
-    private void deleteindex(int idx) {
+    private void deleteIndex(int idx) {
         if (deleted == NULL) {
             deleted = idx;
             n[idx].dnext = NULL;
@@ -85,5 +85,98 @@ public class AryLinkedList<E> {
         }
     }
 
+    public void addLast(E obj) {
+        if (head == NULL)
+            addFirst(obj);
+        else {
+            int ptr = head;
+            while (n[ptr].next != NULL)
+                ptr = n[ptr].next;
+            int rec = getInsertIndex();
+            if (rec != NULL) {
+                n[ptr].next = crnt = rec;
+                n[rec].set(obj, NULL);
+            }
+        }
+    }
+
+    public void removeFirst() {
+        if (head != NULL) {
+            int ptr = n[head].next;
+            deleteIndex(head);
+            head = crnt = ptr;
+        }
+    }
+
+    public void removeLast() {
+        if (head != NULL) {
+            if (n[head].next == NULL)
+                removeFirst();
+            else {
+                int ptr = head;
+                int pre = head;
+
+                while (n[ptr].next != NULL) {
+                    pre = ptr;
+                    ptr = n[ptr].next;
+                }
+                n[pre].next = NULL;
+                deleteIndex(ptr);
+                crnt = pre;
+            }
+        }
+    }
+
+    public void remove(int p) {
+        if (head != NULL) {
+            if (p == head)
+                removeFirst();
+            else {
+                int ptr = head;
+
+                while (n[ptr].next != p) {
+                    ptr = n[ptr].next;
+                    if (ptr == NULL) return;
+                }
+                n[ptr].next = NULL;
+                deleteIndex(p);
+                n[ptr].next = n[p].next;
+                crnt = ptr;
+            }
+        }
+    }
+
+    public void removeCurrentNode() {
+        remove(crnt);
+    }
+
+    public void clear() {
+        while (head != NULL)
+            removeFirst();
+        crnt = NULL;
+    }
+
+    public boolean next() {
+        if (crnt == NULL || n[crnt].next == NULL)
+            return false;
+        crnt = n[crnt].next;
+        return true;
+    }
+
+    public void printCurrentNode() {
+        if (crnt == NULL)
+            System.out.println("선택 노드가 없습니다.");
+        else
+            System.out.println(n[crnt].data);
+    }
+
+    public void dump() {
+        int ptr = head;
+
+        while (ptr != NULL) {
+            System.out.println(n[ptr].data);
+            ptr = n[ptr].next;
+        }
+    }
 }
 
