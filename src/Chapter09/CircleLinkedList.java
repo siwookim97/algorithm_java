dedc8896bbf31ae53499bc4047e14fc0d14e5d3f
김시우 <tldn12gh@gmail.com>
2022-02-11 22:44:18 +0900

    포인터를 활용한 연결리스트 구현, 원형으로 구성되어 있어 tail의 값은 head를 가르킨다

--- src/Chapter09/CircleLinkedList.java
+++ src/Chapter09/CircleLinkedList.java
@@ -58,15 +58,49 @@ public class CircleLinkedList <E> {
     }
 
     public void removeFirst() {
-
+        if (head != null) {
+            if (head == tail)
+                head = tail = crnt = null;
+            else {
+                Node<E> ptr = head.next;
+                head = crnt = ptr;
+                tail.next = head;
+            }
+        }
     }
 
     public void removeLast() {
+        if (head != null) {
+            if (head == tail)
+                head = tail = crnt = null;
+            else {
+                Node<E> ptr1 = head;  // 제일 마지막 노드
+                Node<E> ptr2 = head;  // 제일 마지막 노드의 바로 앞
 
+                while (ptr1.next != head) {
+                    ptr2 = ptr1;
+                    ptr1 = ptr1.next;
+                }
+                ptr2.next = head;
+                tail = crnt = ptr2;
+            }
+        }
     }
 
     public void remove(Node p) {
+        if (head != null) {
+            if (head == tail)
+                removeFirst();
+            else {
+                Node<E> ptr = head;
 
+                while (ptr.next != p) {
+                    ptr = ptr.next;
+                    if (ptr == head) return;
+                }
+                ptr.next = p.next;
+            }
+        }
     }
 
     public void removeCurrentNode() {
@@ -74,7 +108,7 @@ public class CircleLinkedList <E> {
     }
 
     public void clear() {
-
+        head = crnt = tail = null;
     }
 
     public boolean next() {
@@ -82,11 +116,19 @@ public class CircleLinkedList <E> {
     }
 
     public void printCurrentNode() {
-
+        if (crnt == null)
+            System.out.println("선택한 노드가 없습니다.");
+        else
+            System.out.println(crnt.data);
     }
 
     public void dump() {
+        Node<E> ptr = head;
 
+        while (ptr != null) {
+            System.out.println(ptr.data);
+            ptr = ptr.next;
+        }
     }
 
 
