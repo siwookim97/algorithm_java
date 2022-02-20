1719ee11bb5bcc86a8902c4b97510cfb7ac0e715
김시우 <tldn12gh@gmail.com>
2022-02-21 00:04:17 +0900

    - 오픈 주소법으로 구현한 해쉬 알고리즘

---
+++ src/Chapter11/OpenHashTester.java
@@ -0,0 +1,118 @@
+package Chapter11;
+
+import java.util.Scanner;
+
+public class OpenHashTester {
+    static Scanner stdIn = new Scanner(System.in);
+
+    static class Data {
+        static final int NO = 1;
+        static final int NAME = 2;
+
+        private Integer no;
+        private String name;
+
+        Integer keyCode() {
+            return no;
+        }
+
+        public String toString() {
+            return name;
+        }
+
+        void scanData(String guide, int sw) {
+            System.out.println(guide + "할 데이터를 입력하세요.");
+
+            if ((sw & NO) == NO) {
+                System.out.print("번호 : ");
+                no = stdIn.nextInt();
+            }
+            if ((sw & NAME) == NAME) {
+                System.out.print("이름 : ");
+                name = stdIn.next();
+            }
+        }
+    }
+
+    enum Menu {
+        ADD("추가"),
+        REMOVE("삭제"),
+        SEARCH("검색"),
+        DUMP("표시"),
+        TERMINATE("종료");
+
+        private final String message;
+
+        static Menu MenuAt(int idx) {
+            for (Menu m : Menu.values()) {
+                if (m.ordinal() == idx)
+                    return m;
+            }
+            return null;
+        }
+
+        Menu(String string) {
+            message = string;
+        }
+
+        String getMessage() {
+            return message;
+        }
+    }
+
+    static Menu SelectMenu() {
+        int key;
+        do {
+            for (Menu m : Menu.values())
+                System.out.printf("(%d) %s  ", m.ordinal(), m.getMessage());
+            System.out.print(" : ");
+            key = stdIn.nextInt();
+        } while (key < Menu.ADD.ordinal() || key > Menu.TERMINATE.ordinal());
+
+        return Menu.MenuAt(key);
+    }
+
+    public static void main(String[] args) {
+        Menu menu;
+        Data data;
+        Data temp = new Data();
+
+        OpenHash<Integer, Data> hash = new OpenHash<Integer, Data>(13);
+
+        do {
+            switch (menu = SelectMenu()) {
+                case ADD:
+                    data = new Data();
+                    data.scanData("추가", Data.NO | Data.NAME);
+                    int k = hash.add(data.keyCode(), data);
+                    switch (k) {
+                        case 1:
+                            System.out.println("그 키 값은 이미 드으록되어 있습니다.");
+                            break;
+                        case 2:
+                            System.out.println("해시 테이블이 가득 찼습니다.");
+                            break;
+                    }
+                    break;
+
+                case REMOVE:
+                    temp.scanData("삭제", Data.NO);
+                    hash.remove(temp.keyCode());
+                    break;
+
+                case SEARCH:
+                    temp.scanData("검색", Data.NO);
+                    Data t = hash.search(temp.keyCode());
+                    if (t != null)
+                        System.out.println("그 키를 갖는 데이터는 " + t + "입니다.");
+                    else
+                        System.out.println("그 데이터가 없습니다.");
+                    break;
+
+                case DUMP:
+                    hash.dump();
+                    break;
+            }
+        } while (menu != Menu.TERMINATE);
+    }
+}
