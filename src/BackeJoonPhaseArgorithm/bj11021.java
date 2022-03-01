395f16090157fdc525ba5f4a735295f5d250b2e3
김시우 <tldn12gh@gmail.com>
2022-03-01 21:04:49 +0900

    - BufferedReader와 BufferedWriterf를 활용한 간단한 for문 출력 알고리즘, BufferedWriter.write를 사용하여 사용자가 원하는 양식으로 출력이 가능하다.

---
+++ src/BackeJoonPhaseArgorithm/bj11021.java
@@ -0,0 +1,25 @@
+package BackeJoonPhaseArgorithm;
+
+import java.io.BufferedReader;
+import java.io.*;
+import java.util.*;
+
+public class bj11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 1; i <= cnt; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            bw.write("Case #" + i + ": "
+                    + (Integer.parseInt(st.nextToken())
+                    + Integer.parseInt(st.nextToken())) + "\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
