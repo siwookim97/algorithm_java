f6062f0dfdb7097a376acf625e1f971a74ece23f
김시우 <tldn12gh@gmail.com>
2022-03-01 23:30:55 +0900

    - BufferedReader와 BufferedWriterf를 활용한 간단한 for문 출력 알고리즘, 별을 계단형식으로 출력하는 알고리즘이다.

---
+++ src/BackeJoonPhaseArgorithm/Star2438.java
@@ -0,0 +1,22 @@
+package BackeJoonPhaseArgorithm;
+
+import java.io.*;
+
+public class Star2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= cnt; i++) {
+            for (int j = 1 ; j <= i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
