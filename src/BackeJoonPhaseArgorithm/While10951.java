93f7620c7b7c9e2680ae745d2f96ef50e5d57343
김시우 <tldn12gh@gmail.com>
2022-03-05 00:00:08 +0900

    - BufferedReade오 StringTokenizer를 이용한 while문 알고리즘

---
+++ src/BackeJoonPhaseArgorithm/While10951.java
@@ -0,0 +1,23 @@
+package BackeJoonPhaseArgorithm;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class While10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+
+        while( (str=br.readLine()) != null ){
+
+            st = new StringTokenizer(str," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+
+        }
+        System.out.print(sb);
+    }
+}
