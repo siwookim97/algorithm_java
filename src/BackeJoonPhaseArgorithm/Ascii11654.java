5d5020546ed0b3852b863ad6e775bd14e02a72b5
김시우 <tldn12gh@gmail.com>
2022-03-08 23:48:58 +0900

    - Ascii 값을 출력

---
+++ src/BackeJoonPhaseArgorithm/Ascii11654.java
@@ -0,0 +1,13 @@
+package BackeJoonPhaseArgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Ascii11654 {
+    public static void main(String[] args) throws IOException {
+        int ascii = System.in.read();
+        System.out.print(ascii);
+    }
+}
