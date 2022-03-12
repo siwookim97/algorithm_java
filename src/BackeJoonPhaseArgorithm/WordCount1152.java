810259ca02fabaa80f1f73b0ccea4c39693e0013
김시우 <tldn12gh@gmail.com>
2022-03-13 03:37:30 +0900

    - 입력받은 문자열(문장)의 띄어쓰기를 기준으로 단어의 수를 구하는 알고리즘

---
+++ src/BackeJoonPhaseArgorithm/WordCount1152.java
@@ -0,0 +1,17 @@
+package BackeJoonPhaseArgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class WordCount1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = st.countTokens();
+
+        System.out.println(n);
+    }
+}
