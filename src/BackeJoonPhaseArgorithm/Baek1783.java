86255aee473a61e699395a8edd3d5e517560b336
김시우 <tldn12gh@gmail.com>
2022-07-05 03:12:18 +0900

    - 기초적인 그리디 알고리즘, 조건을 잘 찾아보고 제한 조건에 따라서 구분되는 부분을 찾아 알고리즘을 세운다

---
+++ src/BackeJoonPhaseArgorithm/Baek1783.java
@@ -0,0 +1,33 @@
+package BackeJoonPhaseArgorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek1783 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int cnt;
+
+        if (N == 1) {
+            cnt = 1;
+        }
+        else if (N == 2) {
+            cnt = Math.min((M + 1) / 2, 4);
+        }
+        else { // N이 3이상
+            if (M < 7) {
+                cnt = Math.min(M, 4);
+            }
+            else {
+                cnt = M - 2;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
