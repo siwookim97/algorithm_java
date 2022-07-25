256595f2ba11e836381f2b356491e483836aa095
김시우 <tldn12gh@gmail.com>
2022-07-25 15:14:34 +0900

    - 프로그래머스 LV.1, Integer.parseInt();

---
+++ src/Programmers_Lev1/Code12925.java
@@ -0,0 +1,15 @@
+package Programmers_Lev1;
+
+public class Code12925 {
+    public static int solution(String s) {
+        int result = 0;
+
+        return result = Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        String s = "-1234";
+
+        System.out.println(solution(s));
+    }
+}
