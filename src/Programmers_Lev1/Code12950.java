633572785f0a790927e9b5914b7e8b43ce0a2b16
김시우 <tldn12gh@gmail.com>
2022-07-28 22:51:23 +0900

    - 프로그래머스 LV.1, 배열의 합

---
+++ src/Programmers_Lev1/Code12950.java
@@ -0,0 +1,21 @@
+package Programmers_Lev1;
+
+public class Code12950 {
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[0].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr1 = {{1}, {2}};
+        int[][] arr2 = {{3}, {4}};
+
+        System.out.println(solution(arr1, arr2));
+    }
+}
