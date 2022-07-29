aa4a79dabadb70ff7160b3cb7f4c4efbefb54b97
김시우 <tldn12gh@gmail.com>
2022-07-29 23:48:28 +0900

    - 프로그래머스 LV.1, 소수를 출력하는 최적화 할고리즘

---
+++ src/Programmers_Lev1/Code12921.java
@@ -0,0 +1,27 @@
+package Programmers_Lev1;
+
+public class Code12921 {
+    public static boolean isPrime(int n) {
+        for (int j = 2; j * j <= n; j++) {
+            if (n % j == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i))
+                answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println(solution(n));
+    }
+}
